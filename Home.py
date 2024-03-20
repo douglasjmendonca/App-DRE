8ada8d460887d4c6505ba60eba19d673049f7b31
Douglas José Mendonça <mendonca_douglas@yahoo.com.br>
2024-03-20 17:16:56 -0300

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,58 @@
+import streamlit as st
+from PIL import Image
+
+st.set_page_config(page_title="Tratamento de Dados para DRE")
+
+st.write("### Tratamento de Dados para DRE - Informações Gerais")
+
+st.markdown(
+    """
+    Bem-vindo ao nosso aplicativo especializado em tratamento e transformação de dados para o banco de dados da DRE. 
+    Reconhecemos a importância de processar informações de forma eficiente e segura, e por isso, 
+    desenvolvemos uma plataforma segmentada em módulos que representam diferentes etapas desse tratamento.
+
+  ### Movimentação de Estoque:
+    - [Clique aqui](https://drive.google.com/uc?export=download&id=1MvaaoHyCcB85T6LuO1EWmGl259G6P2Qz) para baixar a planilha com
+    modelo dos dados de entrada para esse processo.
+
+  """
+  """
+  ### Conteúdo de Programação:
+    - [Clique aqui](https://drive.google.com/uc?export=download&id=1-SvAZs0izjIHUarTWf0bLhPnE6X9KJ6c) para baixar a planilha com
+    modelo dos dados de entrada para esse processo.
+  
+  """
+  """
+  ### Acompanhamento de Terceirizadas:
+    - [Clique aqui](https://drive.google.com/uc?export=download&id=1YRy80_Jdft3rIht9nWNZhUACmD1ZvwAH) para baixar a planilha com
+    modelo dos dados de entrada para esse processo.
+  
+  """
+  """
+  ### Faturamento e Impostos:
+    - [Clique aqui](https://drive.google.com/uc?export=download&id=1G6r7551I3NDW68m2apCBWeAUuMeLdDr1) para baixar a planilha com
+    modelo dos dados de entrada para esse processo.
+  
+  """
+  """
+  ### Despesas Contábeis:
+    - [Clique aqui](https://drive.google.com/uc?export=download&id=1mk_Co3eCiXQjMLVR9LvswbpsGfNSob_U) para baixar a planilha com
+    modelo dos dados de entrada para esse processo.
+  
+  """
+  """
+  ### Pagamentos:
+    - [Clique aqui](https://drive.google.com/uc?export=download&id=1bjXMhg_jJ4t5d0FPkG0sF_2HofDtLw2p) para baixar a planilha com
+    modelo dos dados de entrada para esse processo.
+    
+  """ 
+  """
+  ### União dos Arquivos da DRE:
+    Esse processo faz a junção de todos os arquivos gerados dos processos  anteriores em apenas uma arquivo. Para isto, bata apenas
+    carregar os arquivos dos processos e depois clicar no botão iniciar. Depois de algum tempo será gerado um arquivo final com todos 
+    os arquivos unidos.
+
+  """ 
+)
+
+
